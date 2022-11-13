3534ea2f682b624f0464a2be281a0d25e2742098
mbernes <mbernes3@gmail.com>
2022-11-13 21:44:27 +0200

    Comm1

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const express = require('express');
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Node.js is amazing! ⚡️');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
